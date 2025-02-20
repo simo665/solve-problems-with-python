9b2bc929e001ef7771ef33838e884a28c2f442da
simo <jumpursimo12@gmail.com>
2025-02-20 17:39:43 +0100

    problem solved

---
+++ two_sum/main.py
@@ -0,0 +1,59 @@
+class Solution:
+    def __init__(self, array, target):
+        self.array = array
+        self.target = target
+        self.get_solution()
+    
+    @property
+    def array(self):
+        return self._array
+
+    @array.setter
+    def array(self, array):
+        if not array:
+            raise ValueError("Missing 'array' values")
+        
+        if not isinstance(array, list):
+            array = array.strip().split(",")
+        
+        if len(array) < 2:
+            raise ValueError("The array must contain at least two values.")    
+        
+        int_list = []
+        try:
+            int_list = [int(value) for value in array]
+        except ValueError:
+            raise ValueError(f"Invalid input: '{value}' is not an integer.")
+        self._array = int_list
+    
+    @property
+    def target(self):
+        return self._target
+
+    @target.setter
+    def target(self, target):
+        if not target:
+            raise ValueError("Missing 'target' values")
+        try:
+            int(target)  
+        except ValueError:
+            raise ValueError(f"Invalid input: '{value}' is not an integer.")
+        self._target = int(target)
+    
+    def get_solution(self):
+        nums = self.array
+        for num in nums:
+            for num2 in nums:
+                if num == num2:
+                    continue 
+                sum = num + num2
+                if sum == self.target:
+                    return [nums.index(num), nums.index(num2)]
+        return None
+
+def main():
+    list = [1,3,5,6,9,2]
+    print(Solution(list, 5).get_solution())
+
+if __name__ == "__main__":
+    main()
---
+++ two_sum/two_sum.md
@@ -0,0 +1,25 @@
+# Two Sum
+
+So an this problem solvation i need to provide an array of numbers and a target numbers, and i need to find two numbers from that array that when summed together it will equal the target number.
+
+### Example:
+```py
+numbers = [0,4,5,6,8]
+```
+#### and a target number:
+```py
+target = 10
+```
+#### So logically the output should be:
+```bash
+[1,2]
+```
+### Explanation:
+#### We have: 
+```py
+numbers[1] + numbers[2] == target
+```
+#### so the output will be:
+```bash
+[1,2]
+```
